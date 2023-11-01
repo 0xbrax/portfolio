e13582aaf67d0f45ee77de53afb5c10ecb21a371
0xbrax <marco.braccini@live.it>
2023-11-01 01:04:46 +0100

    build test

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,21 @@
 // https://vitejs.dev/config/
 
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
-export default defineConfig({
-  plugins: [
-    vue()
-  ],
-  resolve: {
-    alias: {
-      '@': '/src',
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), 'VUE_APP_');
+
+  return {
+    plugins: [
+      vue()
+    ],
+    resolve: {
+      alias: {
+        '@': '/src',
+      },
     },
-  },
-  assetsInclude: ['**/*.glb'],
-  publicPath: process.env.NODE_ENV === 'production' ? env.BASE_URL : '/',
+    assetsInclude: ['**/*.glb'],
+    publicPath: process.env.NODE_ENV === 'production' ? env.BASE_URL : '/',
+  }
 });
