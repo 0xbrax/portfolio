1592cbcb2d58b79de2c4277e3c1d8bf7a7dc33aa
0xbrax <marco.braccini@live.it>
2023-11-01 01:42:00 +0100

    fix config routing single page app

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,16 @@
 // https://vitejs.dev/config/
 
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), 'VUE_APP_');
-
-  return {
-    plugins: [
-      vue()
-    ],
-    resolve: {
-      alias: {
-        '@': '/src',
-      },
+export default defineConfig({
+  plugins: [
+    vue()
+  ],
+  resolve: {
+    alias: {
+      '@': '/src',
     },
-    assetsInclude: ['**/*.glb'],
-    publicPath: mode === 'production' ? env.VUE_APP_BASE_URL : '/',
-  }
+  },
+  assetsInclude: ['**/*.glb']
 });
