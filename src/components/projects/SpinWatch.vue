dbd63beeff60da2d687dd88b799779105b137366
0xbrax <marco.braccini@live.it>
2023-12-10 13:18:05 +0100

    added images + animation upgrade

--- src/components/projects/SpinWatch.vue
+++ src/components/projects/SpinWatch.vue
@@ -4,11 +4,13 @@
             <div @click="spin()" ref="refGame" id="game" class="d-flex justify-ctr align-ctr relative">
                 <div 
                     class="symbol-container"
-                    v-for="el in new Array(REEL_LENGTH)" 
-                    :key="el"
+                    v-for="(el, i) in new Array(REEL_LENGTH)"
+                    :key="i"
                     :ref="(container) => refSymbolContainers.push(container)"
                 >
-                    <div class="symbol" :ref="(symbol) => refSymbols.push(symbol)"></div>
+                    <div class="symbol" :ref="(symbol) => refSymbols.push(symbol)">
+                        <img :src="assetsUrl(`projects/spinwatch/image/${SYMBOLS[i]}.png`)" class="w-100" />
+                    </div>
                 </div>
 
                 <svg>
@@ -16,7 +18,7 @@
                         ref="refProgress"
                         :cx="progressDimension" 
                         :cy="progressDimension" 
-                        :r="progressDimension" 
+                        :r="progressDimension && progressDimension - 10" 
                     />
                 </svg>
             </div>
@@ -26,7 +28,7 @@
 
 <script>
 import { onMounted, ref } from 'vue';
-import { getRandomNumber } from '@/assets/js/utils.js';
+import { assetsUrl, getRandomNumber } from '@/assets/js/utils.js';
 
 export default {
     name: 'SpinWatch',
@@ -40,20 +42,38 @@ export default {
         const REEL_LENGTH = 12;
         const DEG_GAP = 30;
 
+        const SYMBOLS = ['o', 'bar-1', 'x-w', 'bar-3', 'x-b', 'bar-2', 'o', 'bar-3', 'x-w', 'bar-1', 'x-b', 'bar-2'];
+        let winIndex;
+        let winSymbol;
+
+        let isFirstPlay = true;
         let isLoaded = false;
+        let isPlaying = false;
 
         const spin = () => {
-            // TODO Reset degree if position is === start position
-            if (!isLoaded) return;
+            // TODO Reset degree if position is === start position => JS MAX NUMBER (degree > max when play hard)...
+            if (!isLoaded || isPlaying) return;
 
-            let animDuration = getRandomNumber(10, 15);
-            animDuration = animDuration * 1000;
+            if (winIndex != null) refSymbols.value[winIndex].classList.remove('spin-end');
 
-            const iteration = 12 * getRandomNumber(1, 3);
+            isPlaying = true;
+            let animDuration = getRandomNumber(15, 20);
+            animDuration = animDuration * 100;
+
+            const iteration = 12 * getRandomNumber(1, 2);
             let multiplier = getRandomNumber(0 + iteration, 11 + iteration);
+            winIndex = multiplier - iteration;
+            winSymbol = SYMBOLS[winIndex];
             multiplier = multiplier * DEG_GAP;
 
+            console.log('LOG.................', winIndex, winSymbol)
+
             reelAnimation(animDuration, multiplier);
+
+            setTimeout(() => {
+                isPlaying = false;
+                refSymbols.value[winIndex].classList.add('spin-end');
+            }, animDuration);
         }
 
         const reelAnimation = (duration, multiplier) => {
@@ -76,7 +96,7 @@ export default {
                     degEndSymbol -= DEG_GAP;
                 }
 
-                const animKeyContainerFrames = [
+                const animContainerKeyframes = [
                     {
                         transform: `translate(-50%, -100%) rotate(${degStartContainer + 'deg'})`
                     },
@@ -84,11 +104,11 @@ export default {
                         transform: `translate(-50%, -100%) rotate(${degEndContainer + 'deg'})`
                     }
                 ];
-                const animKeySymbolFrames = [
+                const animSymbolKeyframes = [
                     {
                         transform: `translate(-50%, -50%) rotate(${degStartSymbol + 'deg'})`,
-                        top: '100%',
-                        height: '80%'
+                        top: isFirstPlay ? 'calc(15% + 20px)' : '100%',
+                        height: isFirstPlay ? '30%' : '80%'
                     },
                     {
                         top: 'calc(15% + 20px)',
@@ -112,22 +132,21 @@ export default {
                     fill: 'forwards'
                 };
 
-                refSymbolContainers.value[i].animate(animKeyContainerFrames, animProperties);
-                refSymbols.value[i].animate(animKeySymbolFrames, animProperties);
+                refSymbolContainers.value[i].animate(animContainerKeyframes, animProperties);
+                refSymbols.value[i].animate(animSymbolKeyframes, animProperties);
+
+                setTimeout(() => {
+                    if (i === winIndex) refSymbolContainers.value[i].style.zIndex = '2';
+                    else refSymbolContainers.value[i].style.zIndex = '1';
+                }, duration / 2);
             }
+
+            isFirstPlay = false;
         }
 
 
 
-        // TODO remove
-        const getRandomColor = () => {
-            const letters = '0123456789ABCDEF';
-            let color = '#';
-            for (let i = 0; i < 6; i++) {
-                color += letters[Math.floor(Math.random() * 16)];
-            }
-            return color;
-        }
+        // UTILS
 
         const getRotationDegrees = (matrix) => {
             const values = matrix.match(/matrix\(([^)]+)\)/)[1].split(',').map(parseFloat);
@@ -168,35 +187,26 @@ export default {
 
                 refSymbolContainers.value[i].style.transform = `translate(-50%, -100%) rotate(${degStartContainer + 'deg'})`;
                 refSymbols.value[i].style.transform = `translate(-50%, -50%) rotate(${degStartSymbol + 'deg'})`;
-                refSymbols.value[i].style.top = '100%';
-                refSymbols.value[i].style.height = '80%';
-
-                refSymbols.value[i].style.backgroundColor = getRandomColor();
+                refSymbols.value[i].style.top = 'calc(15% + 20px)';
+                refSymbols.value[i].style.height = '30%';
             }
 
 
 
+            /////////////////// TODO
+            let progress = 120;
 
-            let progress = 145;
-
-            const circleDimension = 2 * progressDimension.value * Math.PI;
+            const circleDimension = 2 * (progressDimension.value - 10) * Math.PI;
             const circleDegree = (progress / 360) * circleDimension;
             refProgress.value.style.strokeDasharray = `${circleDegree}, ${circleDimension - circleDegree}`;
             refProgress.value.style.strokeDashoffset = circleDegree / 2;
-
-            console.log(circleDimension, circleDegree)
-
-
-            /*console.log(refSymbolContainers.value[0])
-
-            refSymbolContainers.value[0].addEventListener('animationend', () => {
-                console.log('WEEE')
-            });*/
 
             isLoaded = true;
         });
 
         return {
+            assetsUrl,
+            SYMBOLS,
             refGame,
             refSymbolContainers,
             refSymbols,
@@ -236,24 +246,21 @@ export default {
 
 .symbol-container {
     position: absolute;
-    z-index: 1;
-    width: 4px;
     height: 50%;
-    background: yellow;
     top: 50%;
     left: 50%;
     transform-origin: 50% 100%;
 }
 .symbol {
     aspect-ratio: 1;
-
     position: absolute;
     left: 50%;
-
-    box-shadow: inset 0px 20px 0px 0px black;
+    transition: 0.3s ease-in-out;
 }
-
-
+.symbol.spin-end {
+    background-color: rgba(255, 255, 255, 0.8);
+    box-shadow: 0px 0px 5px 5px rgba(255, 255, 255, 0.8);
+}
 
 svg {
     width: 100%;
