e571452a315a04205c0f5e3e9b986153cb3fb879
0xbrax <marco.braccini@live.it>
2024-10-29 15:23:00 +0100

    it works on my machine

--- src/experience/InterestPoints.js
+++ src/experience/InterestPoints.js
@@ -58,7 +58,6 @@ export default class InterestPoints {
             plateGroup.add(plateBody, plateText);
             pointGroup.cProps = { ...el };
             pointGroup.add(pole, plateGroup);
-            pointGroup.rotation
             this.instanceGroup.add(pointGroup);
         });
     }
@@ -66,7 +65,6 @@ export default class InterestPoints {
     setSphericalPosition(object, selectedPoint) {
         const position = new THREE.Vector3(...Object.values(selectedPoint));
         object.position.copy(position);
-        object.position.y = 4
 
         const direction = new THREE.Vector3().subVectors(new THREE.Vector3(0, 0, 0), position).normalize();
         const up = new THREE.Vector3(0, 0, 1);
